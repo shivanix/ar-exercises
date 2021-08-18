18fb3f55792e5482930b30b7b0bb4127c0a5eec8
shivanix <xuxu@LAPTOP-9J0KT8BE.localdomain>
2021-08-18 16:35:59 -0400

    Use AR's create class meth multiple times to create 3 stores in the db; output the num of stores using AR's count method

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,35 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+#Create method creates an object (or multiple objects) and saves it to the database, if validations pass. 
+#The resulting object is returned whether the object was saved successfully to the database or not.
+
+#ActiveRecord's count method will perform a SQL COUNT query
+#equivalent to SELECT COUNT(*) FROM "stores"
+
+
+burnaby = Store.create(
+  name: 'burnaby',
+  annual_revenue: '300000',
+  mens_apparel: TRUE,
+  womens_apparel: TRUE
+)
+
+richmond = Store.create(
+  name: 'richmond',
+  annual_revenue: '1260000',
+  mens_apparel: TRUE,
+  womens_apparel: TRUE
+)
+
+gastown = Store.create(
+  name: 'gastown',
+  annual_revenue: '190000',
+  mens_apparel: TRUE,
+  womens_apparel: TRUE
+)
+ 
+num_of_stores = Store.count
+
+puts "The number of stores are : #{num_of_stores}"
